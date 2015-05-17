824ab11c85b47fa274af2868bcb75c3c1a04d8c3
tovain10071991 <tovain1991@gmail.com>
2015-05-17 10:52:38 +0800

    add error handlers about ptrace and wait

---
+++ dbg.h
@@ -0,0 +1,45 @@
+#ifndef _DBG_H_
+#define _DBG_H_
+
+#include "proc.h"
+#include "type.h"
+#include <fstream>
+#include <udis86.h>
+
+using namespace std;
+using namespace skyin;
+
+#define traceMin 10
+
+#define isBranch(ud_obj) \
+	(ud_insn_mnemonic(ud_obj)==UD_Icall || ud_insn_mnemonic(ud_obj)==UD_Iiretw || ud_insn_mnemonic(ud_obj)==UD_Iiretd || ud_insn_mnemonic(ud_obj)==UD_Iiretq || (ud_insn_mnemonic(ud_obj)>=UD_Ijo && ud_insn_mnemonic(ud_obj)<=UD_Ijmp) || ud_insn_mnemonic(ud_obj)==UD_Iret || ud_insn_mnemonic(ud_obj)==UD_Iretf)
+
+extern ofstream fdebugger;
+extern ofstream fdis;
+extern int status;
+extern UINT_T ret;
+
+namespace skyin {
+
+class Process;
+
+class Debugger {
+private:
+	Process* process;
+	void* trace;
+	UINT_T traceEnd;
+	ud_t ud_obj;
+	const ud_operand_t* ud_opr;
+	bool contBreak(UINT_T addr);
+	bool contWrite(UINT_T addr);
+	bool singleStep();
+	bool readTrace();
+public:
+	Debugger(Process* process);
+	bool readData(UINT_T addr, size_t size, void* data);
+	bool updateTrace();
+};
+
+}
+
+#endif
---
+++ proc.h
@@ -0,0 +1,48 @@
+#ifndef _PROC_H_
+#define _PROC_H_
+
+#include <string>
+#include <gelf.h>
+#include <vector>
+#include <sys/user.h>
+#include "type.h"
+#include <fstream>
+#include "dbg.h"
+
+using namespace std;
+
+extern ofstream fmodule;
+
+
+namespace skyin {
+
+class Debugger;
+
+class Process {
+	friend class Debugger;
+private:
+	class Module {
+	public:
+		std::string		path;
+		Elf*		elf;
+		GElf_Ehdr	ehdr;
+		UINT_T		baseAddr;
+		size_t		size;
+		UINT_T		gotPltAddr;
+		UINT_T		pltAddr;
+		size_t		pltSize;
+	public:
+		Module(UINT_T base, std::string path);
+	};
+	int pid;
+	Debugger* debugger;
+	vector<Module*> modules;
+	struct user_regs_struct regs;
+public:
+	Process(int pid, string mainPath);
+	void initModules();
+};
+
+}
+
+#endif
